92a38ff08dfae6e08d00e9c39ba0ec42150a3461
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-07-27 17:19:04 +0200

    feat (server): morgan

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import helmet from 'helmet'
+import morgan from 'morgan'
 
 const app = express()
 
 // It's best to use Helmet early in your middleware stack so that its headers are sure to be set.
 app.use(helmet())
+app.use(morgan('dev'))
 
 app.listen(8000, () => console.log("Server running on 8000!!!"))
 
