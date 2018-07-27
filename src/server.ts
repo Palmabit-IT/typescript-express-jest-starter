fe3d07bacbe7453c74eac3995d358a1dd1545645
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-07-27 17:14:14 +0200

    security (server): helmet

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express"
+import helmet from 'helmet'
 
 const app = express()
+
+// It's best to use Helmet early in your middleware stack so that its headers are sure to be set.
+app.use(helmet())
 
 app.listen(8000, () => console.log("Server running on 8000!!!"))
 
