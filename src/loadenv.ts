2ce6494bdcda75fb4546033629ad2a067c14a3b6
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-07-30 15:12:47 +0200

    ref (loadenv): use ts interfaces

--- src/loadenv.ts
+++ src/loadenv.ts
@@ -1,2 +1,6 @@
-import dotenv from 'dotenv'
-dotenv.config({path: '.env' + (process.env.NODE_ENV ? '.' + process.env.NODE_ENV : '')})
+import dotenv, {  DotenvOptions } from 'dotenv'
+const dotenvOptions:  DotenvOptions = {
+  path: '.env' + (process.env.NODE_ENV ? '.' + process.env.NODE_ENV : '')
+}
+
+export default dotenv.config(dotenvOptions)
