b558c3ec986df40bf2836a17c428c1688dd8197f
Emanuele Liardo <emanuele.liardo@fleetmatica.com>
2018-07-30 10:23:06 +0200

    feat (server): app port

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ app.use(bodyParser.json(optionsJson)); // for parsing application/json
 // Load routes
 loadRoutes(app)
 
-app.listen(8000, () => console.log("Server running on 8000!!!"))
+const appPort = process.env.APP_PORT || 8000
+app.listen(appPort, () => console.log(`Server running on ${appPort}!!!`))
 
 app.use(errorHandler)
